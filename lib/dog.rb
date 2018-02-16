865bcefb99d581af065002756fd12876dcd532b6
Holden Altaffer <haltaffer@gmail.com>
2018-02-16 02:52:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,5 @@
 class Dog
-  # def initialize(name)
-  #   @name = name
-  # end
-  #
-  # def name=(new_name)
-  #   @name = new_name
-  # end
-  #
-  # def name
-  #   @name
-  # end
+
   def name=(dogs_name)
     @name = dogs_name
   end
@@ -17,4 +7,13 @@ class Dog
   def name
     @name
   end
+
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end 
+
+  def breed
+    @breed
+  end
+
 end
